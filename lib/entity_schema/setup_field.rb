ba8931c04426e575e122e7be5c93d1c49cd0dcd2
Captain Philipp <ph-s@mail.ru>
2018-08-24 01:13:50 +0300

    Abb .public, doc

--- lib/entity_schema/setup_field.rb
+++ lib/entity_schema/setup_field.rb
@@ -1,17 +1,9 @@
 # frozen_string_literal: true
 
 module EntitySchema
-  # TODO: doc
+  # Define or redefine methods for work with Entity fields
   module SetupField
     def setup_field(field, spec)
-      name           = field.name
-      predicate_name = :"#{name}?"
-      setter_name    = "#{name}="
-
-      remove_method(name)           if method_defined?(name)
-      remove_method(predicate_name) if method_defined?(predicate_name)
-      remove_method(setter_name)    if method_defined?(setter_name)
-
       entity_schema.add_field(field)
 
       setup_getter(field)
@@ -20,18 +12,25 @@ module EntitySchema
     end
 
     def setup_getter(field)
-      define_method(field.name) { field.get(self) }
-      private(field.name) unless field.public_getter?
+      m = field.name
+      define_method(m) { field.get(self) }
+      public(m)  if     field.public_getter?
+      private(m) unless field.public_getter?
     end
 
     def setup_setter(field)
-      define_method(:"#{field.name}=") { |value| field.set(self, value) }
-      private(:"#{field.name}=") unless field.public_setter?
+      m = :"#{field.name}="
+      define_method(m) { |value| field.set(self, value) }
+      public(m)  if     field.public_setter?
+      private(m) unless field.public_setter?
     end
 
     def setup_predicate(field)
-      define_method(:"#{field.name}?") { field.get(self) }
-      private(:"#{field.name}?") unless field.public_getter?
+      m = :"#{field.name}?"
+      remove_method(m) if method_defined?(m)
+      define_method(m) { field.get(self) }
+      public(m)  if     field.public_getter?
+      private(m) unless field.public_getter?
     end
   end
 end
